0846cbd920764dbfe3571656b0cda708fe4a62b2
hjzm13 <javi24186@gmail.com>
2025-09-05 11:16:10 -0500

    mi primer repositorio. practica 1 unidad 1

---
+++ Unidad1/1-Tarea/Practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+
+internal class Program
+{
+    // Crear un programa que convierta grado celisius a Fahrenheit.
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Practica 1");
+        instructiones();
+    }
+    public static void instructiones()
+    {
+        Console.WriteLine("Agregue la temperatura en Celsius que quiera convertir a fahrenheit");
+        double gradosC = double.Parse(Console.ReadLine());
+        double gradosF = ConvertirCelAFah(gradosC);
+        Console.WriteLine($"La temperatura en farhanheit es {gradosF}");
+    }
+    public static double ConvertirCelAFah(double celisius)
+    {
+        double fahrenheit = 0;
+        // formula = F=(C×1.8)+32
+        fahrenheit = (celisius * 1.8) + 32;
+        return fahrenheit;
+    }
+}
